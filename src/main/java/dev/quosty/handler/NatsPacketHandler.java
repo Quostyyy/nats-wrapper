7f5b70c32ed87c1886d944137d57bba514e3082a
Quostyyy <patrys.kowalczys@interia.pl>
2023-03-18 00:17:30 +0100

    General code cleanup

--- src/main/java/dev/quosty/handler/NatsPacketHandler.java
+++ src/main/java/dev/quosty/handler/NatsPacketHandler.java
@@ -5,6 +5,7 @@ import dev.quosty.listener.NatsListener;
 import dev.quosty.packet.NatsPacket;
 
 public interface NatsPacketHandler {
+
     <T extends NatsPacket> void registerListener(NatsListener<T> listener);
 
     <T extends NatsPacket> void sendPacket(String channel, T packet);
