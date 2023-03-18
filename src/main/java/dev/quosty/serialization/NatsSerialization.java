f36a0255540a4ce11287efa9e021cf26fd99113f
neziw <kolopajlopiotr@gmail.com>
2023-03-18 18:15:35 +0100

    add final keyword

--- src/main/java/dev/quosty/serialization/NatsSerialization.java
+++ src/main/java/dev/quosty/serialization/NatsSerialization.java
@@ -9,16 +9,16 @@ import java.io.ObjectOutputStream;
 public class NatsSerialization {
 
     public static byte[] serialize(Object object) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(object);
         objectOutputStream.flush();
         return byteArrayOutputStream.toByteArray();
     }
 
     public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
         return objectInputStream.readObject();
     }
 }
